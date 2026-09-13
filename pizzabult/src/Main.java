e042c3ecb2a51db8a173c43179c430e9e6921139
Aluana <Muslimova>
2026-09-13 21:37:43 +0500

    Final commit

--- pizzabult/src/Main.java
+++ pizzabult/src/Main.java
@@ -1,4 +1,16 @@
-void main() {
+public class Main {
+    public static void main(String[] args) {
+        Director director = new Director();
 
+        PizzaBuilder pepperoniBuilder = new PepperoniBuilder();
+        Pizza pepperoniPizza = director.construct(pepperoniBuilder);
+        System.out.println("Order for Azi:" + pepperoniPizza);
+
+        PizzaBuilder margaritaBuilder = new MargaritaBuilder();
+        Pizza margaritePizza = director.construct(margaritaBuilder);
+        System.out.println("Order for Meeaovv:" + margaritePizza);
+
+    }
 }
+
 
